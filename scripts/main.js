c8d239de507643b91dfc88d2068cd5dae58c6db5
zinidtv <zinidgames@gmail.com>
2025-08-26 20:47:42 -0300

    Menu não fixo

--- scripts/main.js
+++ scripts/main.js
@@ -43,20 +43,6 @@ document.getElementById("prev").addEventListener("click", () => {
 
 updateCards();
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
-    });
-});
-
 // CÓDIGO DO FORMULÁRIO DE MENSAGEM
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById('form-contato');
